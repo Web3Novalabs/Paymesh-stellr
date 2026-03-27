62e61709f1023e0dc451be85b4f69d8c8882dab7
Emmzyemms <friyoemmy@gmail.com>
2026-03-27 22:10:18 +0100

    Test/Add tests for admin transfer edge cases

--- contract/contracts/hello-world/src/lib.rs
+++ contract/contracts/hello-world/src/lib.rs
@@ -531,3 +531,7 @@ mod fundraising_reset_test;
 #[cfg(test)]
 #[path = "tests/group_name_validation_test.rs"]
 mod group_name_validation_test;
+
+#[cfg(test)]
+#[path = "tests/admin_transfer_test.rs"]
+mod admin_transfer_test;
